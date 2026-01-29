f7cd013df042670ca5e9a023a9b20e594caee167
Aadil Shaikh <aadilshaikh897265@gmail.com>
2026-01-29 12:05:22 +0530

    Finetrack v2 clean dashboard, charts, alerts

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,10 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore, doc, setDoc } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
   apiKey: "AIzaSyA5sgN9zpdUn1CoIhJusdmN9wuCJqs_4NI",
   authDomain: "personal-finance-tra.firebaseapp.com",
